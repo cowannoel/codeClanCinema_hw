b7ae27c7b0b9a70bffd8bf44de521f8d9750fedb
cowannoel <cowannoel@gmail.com>
2020-01-31 20:33:45 +0000

    Initial commit

---
+++ start_point/console.rb
@@ -0,0 +1,61 @@
+require_relative('models/ticket')
+require_relative('models/customer')
+require_relative('models/film')
+
+require('pry-byebug')
+
+
+
+  film1 = Film.new({
+    'title' => 'ET',
+    'price' => 12
+  })
+  film1.save()
+
+
+  film2 = Film.new({
+    'title' => 'Jaws',
+    'price' => 12
+  })
+  film2.save()
+
+  film3 = Film.new({
+    'title' => 'Close Encounters',
+    'price' => 12
+  })
+  film3.save()
+
+  customer1 = Customer.new({
+    'name' => 'Noel',
+    'funds' => 50
+    })
+    customer1.save()
+
+  customer2 = Customer.new({
+    'name' => 'Becky',
+    'funds' => 100
+  })
+  customer2.save()
+
+  customer3 = Customer.new({
+    'name' => 'Jude',
+    'funds' => 20
+  })
+  customer3.save()
+
+  ticket1 = Ticket.new({'film_id' => film1.id, 'customer_id' => customer1.id})
+  ticket2 = Ticket.new({'film_id' => film1.id, 'customer_id' => customer2.id})
+  ticket3 = Ticket.new({'film_id' => film2.id, 'customer_id' => customer2.id})
+  ticket4 = Ticket.new({'film_id' => film3.id, 'customer_id' => customer3.id})
+  ticket5 = Ticket.new({'film_id' => film1.id, 'customer_id' => customer3.id})
+  ticket6 = Ticket.new({'film_id' => film2.id, 'customer_id' => customer2.id})
+
+  ticket1.save()
+  ticket2.save()
+  ticket3.save()
+  ticket4.save()
+  ticket5.save()
+  ticket6.save()
+
+binding.pry
+nil
---
+++ start_point/models/ticket.rb
@@ -0,0 +1,24 @@
+require_relative("../db/sql_runner")
+
+class Ticket
+  attr_reader :id
+  attr_accessor :film_id, :customer_id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @film_id = options['film_id'].to_i
+    @customer_id = options['customer_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO tickets (film_id, customer_id)
+    VALUES ($1, $2)
+    RETURNING id"
+    values = [@film_id, @customer_id]
+    ticket = SqlRunner.run(sql, values).first
+    @id = ticket['id'].to_i
+  end
+
+
+
+end
